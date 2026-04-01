2774c5c3caafbed0b774b943f3653576ec842573
Ramkumar <120459607+ramkumar-pacewisdom@users.noreply.github.com>
2026-04-01 14:52:32 +0530

    Create environment.prod.ts

---
+++ environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    baseURL:'https:/qa.elevate-apis.shikshalokam.org/',
+    parentURL:"https://qa.elevate-sandbox.shikshalokam.org/",
+    prefix:''
+};
