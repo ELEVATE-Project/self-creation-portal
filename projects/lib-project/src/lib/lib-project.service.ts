3dac5ed648fae3c87a0b30abac68b8a63bed1b3c
ramkumar-pacewisdom <ramkumar.n@pacewisdom.com>
2026-04-29 08:27:21 +0530

    bug fixes

--- projects/lib-project/src/lib/lib-project.service.ts
+++ projects/lib-project/src/lib/lib-project.service.ts
@@ -133,17 +133,20 @@ export class LibProjectService {
           value = this.projectData.certificate.criteria.conditions.C4.conditions[Object.keys(this.projectData.certificate.criteria.conditions.C4.conditions)[0]].value
         }
         else {
-          const keys = Object.keys(this.projectData.certificate.criteria.conditions.C3.conditions);
-          keys.map((task:any) => {
-            this.projectData.certificate.criteria.conditions.C4.expression = this.projectData.certificate.criteria.conditions?.C4?.expression?.length == 0 ? task: this.projectData.certificate.criteria.conditions?.C4?.expression + '||' + task
-            this.projectData.certificate.criteria.conditions.C4.conditions[task]= this.projectData.certificate.criteria.conditions.C3.conditions[task];
-            this.projectData.certificate.criteria.expression = this.projectData?.certificate?.criteria?.expression + "&&C4"
-          })
+          if(this.projectData.certificate.criteria.conditions.C3) {
+            const keys = Object.keys(this.projectData.certificate.criteria.conditions.C3.conditions);
+            keys.map((task:any) => {
+              this.projectData.certificate.criteria.conditions.C4.expression = this.projectData.certificate.criteria.conditions?.C4?.expression?.length == 0 ? task: this.projectData.certificate.criteria.conditions?.C4?.expression + '||' + task
+              this.projectData.certificate.criteria.conditions.C4.conditions[task]= this.projectData.certificate.criteria.conditions.C3.conditions[task];
+              this.projectData.certificate.criteria.expression = this.projectData?.certificate?.criteria?.expression + "&&C4"
+            })
+          }
         }
         this.projectData.certificate.criteria.conditions.C4.validationText = "Add " + value + " evidence" + (parseInt(value) > 1 ? "s":'') + " at any project level"
       }
       if(this.projectData.certificate && this.projectData.certificate.criteria.conditions.C3) {
         this.projectData.certificate.criteria.conditions.C3.validationText = ''; // to remove validation texts if old task were added.
+
         let array = Object.keys(this.projectData.certificate.criteria.conditions.C3.conditions)
         array.forEach((element:any) => {
           this.projectData.certificate.criteria.conditions.C3.validationText = this.projectData.certificate.criteria.conditions.C3.validationText + " Add "+this.projectData.certificate.criteria.conditions.C3.conditions[element].value + " evidence"+  (parseInt(this.projectData.certificate.criteria.conditions.C3.conditions[element].value) > 1 ? "s":'') +" for the task " + this.projectData.tasks.find((task:any)=> task.id == element)?.name+'. '
